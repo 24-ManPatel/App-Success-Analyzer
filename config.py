0d99ba09b397c98223f6be594749140f2efbf56a
24-ManPatel <manpatel240406@gmail.com>
2026-02-21 00:20:47 +0530

    Basic Login Register push

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # Configuration settings for the application - Store database credentials, secret keys, and app settings here
 # Define environment variables and configuration constants that your app needs to run
-
+MONGO_URI = "mongodb+srv://rishitvaitha22_db_user:Rishi123@cluster0.tvmm2vk.mongodb.net/?appName=Cluster0"
+DB_NAME = "app_success_analyzer"
+SECRET_KEY = "supersecretkey"
